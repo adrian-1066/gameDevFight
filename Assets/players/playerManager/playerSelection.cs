f79bf0fb8d4aaa3377338241383deb7662558d21
adrian haythorne <71699779+adrian-1066@users.noreply.github.com>
2023-04-17 21:25:57 +0100

    ahhhhhh

--- Assets/players/playerManager/playerSelection.cs
+++ Assets/players/playerManager/playerSelection.cs
@@ -7,18 +7,26 @@ public class playerSelection : MonoBehaviour
     public int[] m_playerSelections;
     private int m_currentPlayer;
 
+    public GameObject m_selectionMenu;
 
     private void Start()
     {
         m_playerSelections = new int[2];
     }
-    public void CharSelected(int charID)
+    public void CharSelected(charID charID)
     {
-        m_playerSelections[m_currentPlayer] = charID;
+        m_playerSelections[m_currentPlayer] = charID.ID;
         m_currentPlayer++;
         if(m_currentPlayer >= 2)
         {
             //start fight
+            m_currentPlayer = 0;
         }
+    }
+
+    public void startFight()
+    {
+
+
     }
 }
